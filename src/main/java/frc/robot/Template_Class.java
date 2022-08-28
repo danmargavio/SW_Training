7018a8c80cef152532f679937336eea2b37e141c
danmargavio <danmargavio@gmail.com>
2022-08-28 13:45:55 -0500

    Added template file

---
+++ src/main/java/frc/robot/Template_Class.java
@@ -0,0 +1,40 @@
+// Team name / Author Name and acknowledgements/links
+package frc.robot;
+
+// ADD ALL OF THE IMPORTS YOU NEED HERE
+// for example
+// import edu.wpi.first.wpilibj.interfaces.Gyro;
+
+public class Template_Class {
+    // This is where you can include member variable definitions and declarations
+    //
+    public int whatever = 99;  // note that this can be access/changed using Template_Class.whatever
+
+    /**
+   * This is where the class description goes along with information about construction, if needed.
+   * See the example below for the notation used to indicate a constructor parameter
+   * @param example_constructor_parameter This is an example constructor parameter and how to use it.
+   */
+  public Template_Class(int example_constructor_parameter) {
+      // This is where you place the constructor logic, such as declaring specific member variable values
+  }
+    /**
+   * This is a method description that explains what it does and how to use it.
+   * See the example below for the notation used to indicate two different inputs and one output
+   *
+   * @param an_input A description of the 1st input including the type
+   * @param another_input A description of the 2nd input including the type
+   * @return if the method has an output, you can place a description and data type here
+   */
+  public int do_something(int an_input, int another_input) {
+    // Put the method logic here and be sure to include a return if there is one
+    return (an_input + another_input);
+  }
+
+    /**
+   * Every class should include at least one test method
+   */
+  public void test() {
+    // Place the test logic here
+  }
+}
